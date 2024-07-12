35f85c06425f1c2b4d18836dc6cf1ac025b54f33
Awani Kendurkar <awke@odoo.com>
2024-07-12 12:13:31 -0400

    Updated the view and changed list_price field to be stored

--- ny_pw_product/models/product_template.py
+++ ny_pw_product/models/product_template.py
@@ -7,11 +7,11 @@ class ProductTemplate(models.Model):
   price_per_pair = fields.Monetary(string="Price Per Pair")
   list_price = fields.Monetary(string="Sales Price", 
         digits='Product Price',
-        help="Price at which the product is sold to customers.", compute='_compute_sales_price',
-        readonly=False
+        help="Price at which the product is sold to customers.", compute='_compute_list_price',
+        store=True
       )
 
   @api.depends("pair_per_case", "price_per_pair")
-  def _compute_sales_price(self):
+  def _compute_list_price(self):
     for record in self:
       record.list_price = record.pair_per_case * record.price_per_pair
